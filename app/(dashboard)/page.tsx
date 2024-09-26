d2f62b7e6aaa2c36790608d4270a20c712465209
lassenextkbh <lass365f@edu.next.kbh.dk>
2024-09-26 17:17:24 +0200

    Refactor sign-in page layout, add logo image, and implement authentication protection

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p>This is an authenticated route.</p>
+      <UserButton />
     </div>
   );
 }
