f79ce6e712b75e657772b6ce3b1e9698c7e7d1a4
lassenextkbh <lass365f@edu.next.kbh.dk>
2024-09-27 14:42:50 +0200

    Refactor ESLint configuration to disable unused variable rule. Also began work on the navigation, header and nav-button components for the dashboard.

---
+++ components/nav-button.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  href: string;
+  label: string;
+  isActive?: boolean;
+};
+
+export const NavButton = ({}: Props) => {
+  return (
+    <div>
+      <p>NavButton</p>
+    </div>
+  );
+};
