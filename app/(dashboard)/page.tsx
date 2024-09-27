7c9995251b604ff1c6becf5bfa5b1b7b21285357
lassenextkbh <lass365f@edu.next.kbh.dk>
2024-09-27 21:45:19 +0200

    Added error handling with appropriate type safety for use-get-accounts hook

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
 // import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { data: accounts } = useGetAccounts();
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
+      <p>Home Page</p>
     </div>
   );
 }
