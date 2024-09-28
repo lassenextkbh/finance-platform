388e5f41f830cf7690859ece640811029dabbaff
lassenextkbh <lass365f@edu.next.kbh.dk>
2024-09-28 17:13:35 +0200

    Refactor welcome message to remove unnecessary exclamation mark

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -8,7 +8,7 @@ export const WelcomeMsg = () => {
   return (
     <div className="space-y-2 mb-4">
       <h2 className="text-2xl lg:text-4xl text-white font-medium">
-        Welcome Back{isLoaded ? ", " : "! "}
+        Welcome Back{isLoaded ? ", " : " "}
         {user?.firstName} 👋
       </h2>
       <p className="text-sm lg:text-base text-[#89B6FD]">
